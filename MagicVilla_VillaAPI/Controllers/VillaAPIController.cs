385def4e2a3be1d0742aef39fd3fd58a1323a9d4
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-10-25 16:35:00 +0300

    DTO

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,19 +1,21 @@
 ﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    [Route("api/VillaAPI")]
+    //[Route("api/controller")]
+    [Route("api/VillaAPI")] 
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-        [HttpGet]   
-        public IEnumerable<Villa> GetVillas()
+       [HttpGet]   
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>
+            return new List<VillaDTO>
             {
-            new Villa { Id=1,Name="Poll View" },
-            new Villa { Id=2,Name="Beach View" }
+            new VillaDTO { Id=1,Name="Poll View" },
+            new VillaDTO { Id=2,Name="Beach View" }
 
             };
         }
