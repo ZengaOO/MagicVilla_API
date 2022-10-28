6a343650bf2a719e9e45fc0aef4821e1af74b293
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-10-28 18:29:45 +0300

    Http PUT in action

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -9,6 +9,10 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [MaxLength(30)]
 
         public string Name { get; set; }
+
+        public int Occupancy { get; set; }  
+
+        public int Sqft { get; set; }   
         
     }
 }
