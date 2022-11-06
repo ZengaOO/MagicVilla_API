b1006d06021f227b7f40d0a63d7844a2a867bb02
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-11-06 19:59:15 +0300

    Standard API Response

--- MagicVilla_VillaAPI/Models/APIResponse.cs
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -4,9 +4,9 @@ namespace MagicVilla_VillaAPI.Models
 {
     public class APIResponse
     {
-        public HttpStatusCode StatusCode { get; set; } 
-        
-        public bool IsSucces { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+        public bool IsSucces { get; set; } = true;
 
         public List<string> ErrorMessages { get; set; }
 
